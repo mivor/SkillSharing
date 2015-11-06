2ebfb6610a9e1422c4986556518001a52deda9ec
razvanbh <razvy.rotaru@gmail.com>
2015-11-06 17:03:53 +0200

    Added web project

---
+++ SkillSharing.Web/app/app.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app', ['ngRoute', 'app.core', 'app.dashboard'])
+        .config(config);
+
+    config.$inject = ['$routeProvider'];
+
+    function config($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/dashboard/dashboard.html',
+                controller: 'Dashboard',
+                controllerAs: 'vm'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+})();
---
+++ SkillSharing.Web/app/core/dataservice.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .factory('dataservice', dataservice);
+
+    dataservice.$inject = ['$http'];
+
+    function dataservice($http) {
+        return {
+            getData: getData
+        };
+
+        function getData() {
+            return $http.get('/api/data')
+                .then(getDataComplete);
+
+            function getDataComplete(response) {
+                return response.data.results;
+            }
+        }
+    }
+})();
